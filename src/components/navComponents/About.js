830cf6e12c15f035d07fc091c1a8cfe8ef57b5e5
Amaka Konwea <lawrahcollections@gmail.com>
2024-03-28 13:17:43 +0000

    Updated about section

--- src/components/navComponents/About.js
+++ src/components/navComponents/About.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import Manual from '../../assets/question.png'
-// import { FaExclamationCircle } from "react-icons/fa";
+import { FaExclamationCircle } from "react-icons/fa";
 
 
 const About = () => {
+
+  const aboutData = [
+    {
+      icon: <FaExclamationCircle className="w-8 h-8 mr-2" />,
+      title: "Since 2001",
+      text: "Afec Cup was first held in 2001. The tournament was started by the football clubs",
+    },
+    {
+      icon: <FaExclamationCircle className="w-8 h-8 mr-2" />,
+      title: "Our Organisation",
+      text: "Our organization consists of eight full-time employed staff and during the tournament more than 2500 people work.",
+    },
+    {
+      icon: <FaExclamationCircle className="w-8 h-8 mr-2" />,
+      title: "Address",
+      text: "Through the years, over a million players from a total of 147 countries have participated in the tournament.",
+    },
+    {
+      icon: <FaExclamationCircle className="w-8 h-8 mr-2" />,
+      title: "More than a football tournament",
+      text: "Encourage and back on moral education and academic scholarships for under 15-18 to overseas.Getting involved in children’s",
+    }
+  ]
+
   return (
     <>
     <div className='flex flex-col md:flex-row justify-between px-8'>
@@ -24,42 +48,19 @@ const About = () => {
       <p className='px-40'>It’s the teams, and participants from around the world that make the tournament unique. A meeting place for the world’s youth, irrespective of religion, skin color or nationality, with football as the common denominator. </p>
     </div>
 
-      {/* <div>
-        <ul className='flex justify-between'>
-          <li className='ml-16 flex items-start px-8 '>
-            <FaExclamationCircle className='text-4xl mt-1 mr-4' />
-            <div className=''>
-              <p className='font-bold'>Since 2001</p>
-              <p className=''>Gothia Cup was first held in 1975. The tournament was started by the football clubs </p>
-            </div>
-          </li>
-          <li className='ml-16 flex items-start  px-8'>
-            <FaExclamationCircle className='text-4xl mt-1 mr-4' />
+      <div>
+        {aboutData.map((data, index) => (
+          <div key={index}>
             <div>
-              <p className='font-bold'>Our Organisation</p>
-              <p>Our organization consists of eight full-time employed staff and during the tournament more than 2500 people work.</p>
+              <>{data.icon}</>
             </div>
-          </li>
-        </ul>
+            <div>
+              <h2>{data.title}</h2>
+              <h2>{data.text}</h2>
+            </div>
+          </div>
+        ))}
     </div>
-    <div>
-        <ul className='flex justify-between'>
-          <li className='ml-16 flex items-start px-8 '>
-            <FaExclamationCircle className='text-4xl mt-1 mr-4' />
-            <div>
-              <p className='font-bold'>Address</p>
-              <p className=''>Through the years, over a million players from a total of 147 countries have participated in the tournament.</p>
-            </div>
-          </li>
-          <li className='ml-16 flex items-start px-8'>
-            <FaExclamationCircle className='text-4xl mt-1 mr-4' />
-            <div>
-              <p className='font-bold'>More than a football tournament</p>
-              <p>Encourage and back on moral education and academic scholarships for under 15-18 to overseas.Getting involved in children’s </p>
-            </div>
-          </li>
-        </ul>
-    </div> */}
     </>
   )
 }
